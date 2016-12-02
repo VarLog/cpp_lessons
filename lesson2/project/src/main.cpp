50f73d5e171f10639f0d61f1b87aff044fcf0e05
Maxim Fedorenko <mfedorenko@MacBook-Pro-Maxim.local>
2016-12-02 22:02:05 +0300

    [project] Simplify the test project

--- lesson2/project/src/main.cpp
+++ lesson2/project/src/main.cpp
@@ -3,5 +3,11 @@
 int main(int argc, char** argv) {
     std::cout << "Hello world" << std::endl;
 
+    auto f = [](auto&& i) {
+        std::cout << i << std::endl;
+    };
+
+    f(3);
+
     return 0;
 }
